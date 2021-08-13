b0f1a1f3c417686666d96147d57abef582d68867
Tom Fleet <tomfleet2018@gmail.com>
2021-08-13 11:40:18 +0100

    Combine package level defaults into config.defaults

--- pytoil/cli/docs.py
+++ pytoil/cli/docs.py
@@ -8,10 +8,9 @@ Created: 25/06/2021
 import typer
 from wasabi import msg
 
+from pytoil.config import defaults
+
 app = typer.Typer()
-
-
-PYTOIL_DOCS_URL: str = "https://followtheprocess.github.io/pytoil/"
 
 
 @app.command()
@@ -24,4 +23,4 @@ def docs() -> None:  # pragma: no cover
     $ pytoil docs
     """
     msg.info("Opening pytoil's docs in your browser...", spaced=True)
-    typer.launch(url=PYTOIL_DOCS_URL)
+    typer.launch(url=defaults.PYTOIL_DOCS_URL)
