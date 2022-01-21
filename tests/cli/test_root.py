1ab480e191812b8992e8637216ce37c4758fa0aa
Tom <tomfleet2018@gmail.com>
2022-01-21 18:44:50 +0000

    Add CLI smoke test

---
+++ tests/cli/test_root.py
@@ -0,0 +1,13 @@
+import pytest
+from asyncclick.testing import CliRunner
+
+from pytoil.cli.root import main
+
+
+@pytest.mark.asyncio
+async def test_cli_doesnt_blow_up():
+    runner = CliRunner()
+    result = await runner.invoke(main, ["--help"])
+
+    assert result.exit_code == 0
+    assert "Helpful CLI to automate the development workflow" in result.stdout
